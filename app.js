7ed9cc4cf3ed60710b355b968ccf1181b21f6fea
Mikhail Ponomarenko <mishanya-101@yandex.ru>
2023-04-30 21:42:18 +0400

    fix: errors in tests

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.json());
-
-app.use('/', router);
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = { _id: '644e6bd36d248cd1d30c4acb' };
@@ -15,6 +14,8 @@ app.use((req, res, next) => {
 });
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use('/', router);
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Такой страницы не существует!' });
